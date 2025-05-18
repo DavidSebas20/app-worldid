1ea248117c6c496192e61eb03a754338b09a9e58
DavidSebas20 <davidsebastian20@outlook.com>
2025-05-18 16:51:36 -0500

    Refactor la ruta de inicio de pago; agregar punto y coma faltantes y mejorar la gestión de cookies.

--- app/api/initiate-payment/route.ts
+++ app/api/initiate-payment/route.ts
@@ -1,17 +1,18 @@
-import { cookies } from "next/headers"
-import { type NextRequest, NextResponse } from "next/server"
+import { cookies } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const uuid = crypto.randomUUID().replace(/-/g, "")
+  const uuid = crypto.randomUUID().replace(/-/g, "");
 
   // TODO: Store the ID field in your database so you can verify the payment later
-  cookies().set({
+  const cookieStore = await cookies();
+  cookieStore.set({
     name: "payment-nonce",
     value: uuid,
     httpOnly: true,
-  })
+  });
 
-  console.log(uuid)
+  console.log(uuid);
 
-  return NextResponse.json({ id: uuid })
+  return NextResponse.json({ id: uuid });
 }
